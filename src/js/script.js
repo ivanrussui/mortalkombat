e1e7d5921befac44975f2f2d1dada315cad41d53
Ivan <fenomen_ny@mail.ru>
2021-03-23 21:38:06 +0300

    ajax + form validation + mailer

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,62 @@
 $(document).ready(function () {
+  // vide.js видео фоном на сайте
   $('#app').vide({
-      mp4: 'video/video.mp4',
-      webm: 'video/video.webm',
-      ogv: 'video/video.ogv',
+    mp4: 'video/video.mp4',
+    webm: 'video/video.webm',
+    ogv: 'video/video.ogv',
+  });
+
+  // modal
+  $('#primary').on('click', function () {
+    $('.overlay, #subscribe').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #subscribe, #thanks').fadeOut('slow');
+  });
+
+  // валидаця формы
+  $('#form').validate({
+    rules: {
+      name: 'required',
+      email: {
+        required: true,
+        email: true,
+      },
+    },
+    messages: {
+      name: 'Пожалуйста, введите свое имя',
+      email: {
+        required: 'Пожалуйста, введите свою почту',
+        email: 'Неправильно введена почта',
+      },
+    },
+  });
+
+  // ajax
+  $('#form').submit(function (e) {
+    e.preventDefault();
+
+    // это условие внутри Ajax чтобы письмо пустым не отправлялось
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find('input').val('');
+			$('#subscribe').fadeOut('slow');
+			$('#thanks').fadeIn('slow');
+
+
+      $('form').trigger('reset');
     });
+    return false;
+  });
+
+	$('.modal__close').on('click', function () {
+    $('.overlay, #thanks').fadeOut(500);
+  });
 });
